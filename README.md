4b32b0d29208c64e5aaffb8d1531628d2fe36760
Olena Sinkevych <fuegos1981@gmail.com>
2025-01-06 23:42:24 +0200

    GCS-23 Add tests with Cucumber in training-hours-tracker

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Gym Service
+
+TEST
+
+mvn -pl training-hours-tracker verify -Dgroups="Endpoint_Save_correct_workload" - command fo running cucumber test with tag Endpoint1
+in microservice training-hours-tracker
+
+mvn -pl training-hours-tracker test -Dtest=CucumberRunnerTest - command for running all cucumber tests
+in microservice training-hours-tracker
+
+mvn -pl training-hours-tracker test - command for running all tests in microservice training-hours-tracker
---
+++ training-hours-tracker/src/test/java/com/gym/analytics/automation/steps/GlobalHooks.java
@@ -0,0 +1,50 @@
+package com.gym.analytics.automation.steps;
+
+import com.gym.analytics.TrainingHoursTrackerApplication;
+import com.gym.analytics.model.Trainer;
+import com.gym.analytics.repository.TrainerReportRepository;
+import com.gym.analytics.service.impl.TrainingSummaryManager;
+import io.cucumber.java.Before;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+
+@ExtendWith(SpringExtension.class)
+@CucumberContextConfiguration
+@ContextConfiguration(classes = TrainingHoursTrackerApplication.class)
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GlobalHooks {
+
+    private final static String FIRST_NAME = "John";
+    private final static String LAST_NAME = "Doe";
+    private final static String USERNAME = "John.Doe";
+
+    @Autowired
+    private TrainerReportRepository repository;
+    @Autowired
+    private TrainingSummaryManager manager;
+
+    @Before("@specialHook")
+    public void setupDatabase() {
+        if (repository.findByUsername(USERNAME).isEmpty()) {
+            Trainer trainer = Trainer.builder()
+                    .firstName(FIRST_NAME)
+                    .lastName(LAST_NAME)
+                    .username(USERNAME)
+                    .status(true)
+                    .build();
+
+            manager.addDurationToYearlySummary(trainer, LocalDate.of(2024, 12, 21), 2.0);
+            manager.addDurationToYearlySummary(trainer, LocalDate.of(2024, 11, 21), 3.0);
+
+            repository.save(trainer);
+        }
+    }
+}
